096c42c889233cac4501e9f04cb47644c7674c2d
Kang Young Gyu <kang.yg.dev@gmail.com>
2023-01-02 13:20:36 +0900

    * Version 1.0.4(5) → 1.0.5(6) - Room migration.

--- app/src/main/java/com/kyg/stationareayouthhousing/model/AppDataBase.kt
+++ app/src/main/java/com/kyg/stationareayouthhousing/model/AppDataBase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.kyg.stationareayouthhousing.model.dto.Plan
 
-@Database(entities = [Plan::class], version = 1)
+@Database(entities = [Plan::class], version = 2)
 @TypeConverters(MyTypeConverter::class)
 abstract class AppDataBase : RoomDatabase() {
     abstract fun localService(): LocalService
--- app/src/main/java/com/kyg/stationareayouthhousing/model/AppDataBaseMigration.kt
+++ app/src/main/java/com/kyg/stationareayouthhousing/model/AppDataBaseMigration.kt
@@ -5,10 +5,11 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 
 val MIGRATION_1_2 = object : Migration(1, 2) {
     override fun migrate(database: SupportSQLiteDatabase) {
-        database.also {
-            it.execSQL("ALTER TABLE Plan DROP COLUMN expectedMoveInDate")
-            it.execSQL("ALTER TABLE Plan DROP CONSTRAINT serialNumber PRIMARY")
-            it.execSQL("ALTER TABLE Plan ADD CONSTRAINT address PRIMARY")
+        with(database) {
+            execSQL("CREATE TABLE `Plan_backup` (year TEXT NOT NULL, complexName TEXT NOT NULL, address TEXT NOT NULL, station TEXT NOT NULL, supply TEXT NOT NULL, publicDueDate TEXT NOT NULL, privateDueDate TEXT NOT NULL, PRIMARY KEY (address))")
+            execSQL("INSERT INTO `Plan_backup` (year, address, station, supply, publicDueDate, privateDueDate) SELECT year, address, station, supply, publicDueDate, privateDueDate FROM `Plan`")
+            execSQL("DROP TABLE `Plan`")
+            execSQL("ALTER TABLE `Plan_backup` RENAME TO `Plan`")
         }
     }
 }
